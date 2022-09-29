1ddd1edd7c600d8ec5a828857400481f80b4ea83
jiuntian <jiuntian@gmail.com>
2022-09-29 20:14:51 +0800

    fix video_to_frame script

--- datasets/video/video_to_frame.py
+++ datasets/video/video_to_frame.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 dir_names = os.listdir()[0:-1]
-path = '../EE6222_frames/'
+path = '/raid2/jiuntian/ee6222/darklight/datasets/EE6222_frames/'
 for dir_name in dir_names:
     print("Processing: %s" % dir_name)
     path_dir = os.path.join(path, dir_name)
@@ -10,7 +10,7 @@ for dir_name in dir_names:
     for name in video_names:
         cap = cv2.VideoCapture(os.path.join(dir_name, name))
         if not os.path.exists(path_dir):
-            os.mkdir(path_dir)
+            os.makedirs(path_dir)
         video_path = os.path.join(path_dir, name[:-4])
         i = 1
         if not os.path.exists(video_path):
@@ -25,7 +25,7 @@ for dir_name in dir_names:
             if not ret:
                 print("Video %s Done" % name)
                 break
-            cv2.imwrite(video_path + '\img_{0:05d}.jpg'.format(i), frame)
+            cv2.imwrite(video_path + '/img_{0:05d}.jpg'.format(i), frame)
             i += 1
         # 完成所有操作后，释放捕获器
         cap.release()
