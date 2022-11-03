3f6a6ad5f5d7c7d6d6af24b7c4fa9f2e13826efb
jiuntian <jiuntian@gmail.com>
2022-11-03 10:39:52 +0800

    fix extract test video code

--- utils/video_to_frame_test.py
+++ utils/video_to_frame_test.py
@@ -1,12 +1,14 @@
 import cv2
 import os
 
-video_names = os.listdir('datasets/ee6222/test')
+video_root = 'datasets/ee6222/test'
+video_names = os.listdir(video_root)
 path = 'datasets/EE6222_frames_test/'
 
 path_dir = path
 for name in video_names:
-    cap = cv2.VideoCapture(name)
+    video_mp4_location = os.path.join(video_root, name)
+    cap = cv2.VideoCapture(video_mp4_location)
     if not os.path.exists(path_dir):
         os.makedirs(path_dir)
     video_path = os.path.join(path_dir, name[:-4])
@@ -14,7 +16,7 @@ for name in video_names:
     if not os.path.exists(video_path):
         os.mkdir(video_path)
     if not cap.isOpened():
-        print("Cannot open camera")
+        print(f"Cannot open camera for {video_mp4_location}")
         exit()
     while True:
         ret, frame = cap.read()
